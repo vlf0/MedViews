00433cecd89d1ead4b4fb66b75f9506b1276284e
vlf0 <dr.dnb2000@gmail.com>
2023-08-30 18:44:49 +0700

    The first part of the app is done: foundation created

---
+++ observer/WebApp/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.db import models
+
+
+class DeptChoose(forms.Form):
+    """ This class represent dept choosing form on the page. """
+
+    # List of the all depts from KIS DB
+    depts = Categories.objects.all()
+    depts_tuple = []
+    # Iterations and converting depts string in tuple
+    # Because in requesting only tuple(tuple(str, str), tuple(str, str), ...)
+    for field in depts:
+        depts_tuple.append(tuple([field, field]))
+    # Choice field on the page with ready drop-down list
+    dept_name = forms.ChoiceField(label='Category', choices=depts_tuple)
+
+
---
+++ observer/WebApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+# class HospDepts(models.Model):
+#     """ Represent depts records from KIS DB """
+#
+#     depts =
+
