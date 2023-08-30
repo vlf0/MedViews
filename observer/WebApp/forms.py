0987456369458103d45e92cf090c382384aeb101
vlf0 <dr.dnb2000@gmail.com>
2023-08-30 23:39:26 +0700

    Realized 2 html pages linked each other

--- observer/WebApp/forms.py
+++ observer/WebApp/forms.py
@@ -6,13 +6,16 @@ class DeptChoose(forms.Form):
     """ This class represent dept choosing form on the page. """
 
     # List of the all depts from KIS DB
-    depts = Categories.objects.all()
-    depts_tuple = []
-    # Iterations and converting depts string in tuple
-    # Because in requesting only tuple(tuple(str, str), tuple(str, str), ...)
-    for field in depts:
-        depts_tuple.append(tuple([field, field]))
+    # depts = KIS_Model.objects.all()
+    # depts_tuple = []
+    #
+    # # Iterations and converting depts string in tuple
+    # # Because in requesting only tuple(tuple(str, str), tuple(str, str), ...)
+    # for field in depts:
+    #     depts_tuple.append(tuple([field, field]))
+
     # Choice field on the page with ready drop-down list
-    dept_name = forms.ChoiceField(label='Category', choices=depts_tuple)
+    dept_name = forms.ChoiceField(label='Отделение', choices=(('ПАО', 'ПАО'), ('Педиатрия', 'Педиатрия'),
+                                                              ('Хиругия', 'Хиругия'), ('Кардио', 'Кардио')))
 
 
--- observer/WebApp/views.py
+++ observer/WebApp/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+# My own modules
+from .forms import DeptChoose
 
 
-# Create your views here.
+# Starting page (main)
 def index(request):
-    return render(request=request, template_name='index.html')
+    # Fields to be sending to page (from our forms)
+    depts_list = DeptChoose()
+    return render(request=request, template_name='index.html', context={'depts_list': depts_list})
 
+
+def reporting(request):
+    dept = request.POST.get('dept_name')
+    print(dept)
+    print(type(dept))
+    if dept in ['Педиатрия', 'Хирургия']:
+        return redirect(to='output')
+    messages.error(request, 'Error')
+    return redirect(to='index')
+    # return render(request=request, template_name='output.html')
+
+
+def output(request):
+    return render(request=request, template_name='output.html')
+
+# Redirecting to reporting page
+# def reporting(request):
+#     dept = request.POST.get('dept_name')
+#     print(dept)
+#     for depts in ['Педиатрия', 'Хирургия', 'Кардио']:
+#         if depts == dept:
+#             return redirect(to='reporting')
+#     messages.error(request, 'Error')
+#     return redirect(to='index')
