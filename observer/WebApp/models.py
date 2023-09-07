7d651c879ce21912ee620f341714d943471bca7d
vlf0 <dr.dnb2000@gmail.com>
2023-09-07 19:04:25 +0700

    Rewriting 'operations.py': now based on class instead functions. Added DB model, registered in admin.py.

--- observer/WebApp/models.py
+++ observer/WebApp/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 # Create your models here.
-# class HospDepts(models.Model):
-#     """ Represent depts records from KIS DB """
-#
-#     depts =
+class ConnectingToKIS(models.Model):
+    """ Table for recording and storage
+     data connecting to KIS BataBase
+    """
+    def __str__(self):
+        return self.db
+
+    db = models.CharField(max_length=20, help_text='Input your DataBase name')
+    host = models.CharField(max_length=30, help_text='Input your host name')
+    port = models.CharField(max_length=10, help_text='Input your port')
+    user = models.CharField(max_length=20, help_text='Input DB username')
+    password = models.CharField(max_length=20, help_text='Input your DataBase pass')
+    active = models.BooleanField(default=False, help_text='If you want use some DB - you need turn on the checkbox.')
 
