09353020ea3290be129444df44db1cee254aa7b3
vlf0 <dr.dnb2000@gmail.com>
2023-09-08 17:36:55 +0700

    Fixed problem with DataFrame creating (index of row from DB used before - it was incorrect).

--- observer/WebApp/forms.py
+++ observer/WebApp/forms.py
@@ -15,14 +15,17 @@ class DeptChoose(forms.Form):
     # for field in depts:
     #     depts_tuple.append(tuple([field, field]))
 
-    # Choice field on the page with ready drop-down list
-    dept_name = forms.ChoiceField(label='Отделение', choices=(('dept_1', 'dept_1'), ('dept_2', 'dept_2')))
+    dept_name = forms.CharField(required=True)
+
+    # # Choice field on the page with ready drop-down list
+    # dept_name = forms.ChoiceField(label='Отделение', choices=(('dept_1', 'dept_1'), ('dept_2', 'dept_2')))
 
 
 class ResearchType(forms.Form):
     """ Represent drop-down list of research
         name from KIS DB on the starting page. """
 
-    research_types = forms.ChoiceField(label='Тип незакрытых назначений',
-                                       choices=(('Назначено', 'Назначено'),
-                                                ('Отменено', 'Отменено')))
+    # research_types = forms.ChoiceField(label='Тип незакрытых назначений',
+    #                                    choices=(('Назначено', 'Назначено'),
+    #                                             ('Отменено', 'Отменено')))
+    research_types = forms.CharField(required=True)
