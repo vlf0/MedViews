1ba24c268939361cc289bb3136ebd6f28dcf208d
vlf0 <dr.dnb2000@gmail.com>
2023-09-09 14:22:57 +0700

    Added button returning on the dept choice page; added buttons for date input, appropriate was changed Query class method.

--- observer/WebApp/forms.py
+++ observer/WebApp/forms.py
@@ -1,39 +1,35 @@
+import datetime
 from django import forms
 from django.db import models
 
+# Import my own modules
 from kisdb_connecting.operations import SelectAnswer, Queries
 
 
 class DeptChoose(forms.Form):
-    """ Represent drop-down list of departments
-        name from KIS DB on the starting page. """
-    # Create object that contains all departments from KIS DB
+    """ Represent drop-down list of departments name from KIS DB on the starting page. """
+    # Create object that contains all departments from KIS DB and create list generator
     answer = SelectAnswer(query_text='SELECT name FROM mm.depts d').selecting()
     str_values = [field[0] for field in answer]
 
-    # Defined method that create tuple(tuple(str, str), tuple(str, str), ...)
-    # It will be the choices list on the page
     @staticmethod
     def converting(x):
+        """ Defined method that create tuple of sequence of tuples that send to form the choices list on the page. """
         result = []
         for i in x:
             result.append((i, i))
         return tuple(result)
-    print(converting(str_values))
-
-    # # For testing on the pages
-    # dept_name = forms.CharField(required=True)
-
     # Choice field on the page with ready drop-down list
     dept_name = forms.ChoiceField(label='Отделение', choices=converting(str_values))
 
 
-
 class ResearchType(forms.Form):
-    """ Represent drop-down list of research
-        name from KIS DB on the starting page. """
-
-    # research_types = forms.ChoiceField(label='Тип незакрытых назначений',
-    #                                    choices=(('Назначено', 'Назначено'),
-    #                                             ('Отменено', 'Отменено')))
+    """ Represent drop-down list of research name from KIS DB on the starting page. """
     research_types = forms.CharField(required=True)
+
+
+class DateButtons(forms.Form):
+    """ Represent buttons of date type on the page. It is the 3rd sql condition in query. """
+    from_dt = forms.DateField(required=True, initial=(datetime.date.today()-datetime.timedelta(days=14)))
+    to_dt = forms.DateField(required=True, initial=datetime.date.today())
+
--- observer/observer/urls.py
+++ observer/observer/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('ref_to_type/', views.ref_to_type, name='ref_to_type'),
     path('<str:chosen_dept>/research_type/', views.research_type, name='research_type'),
     path('<str:chosen_dept>/', views.ref_to_output, name='ref_to_output'),
-
-    path('<str:chosen_dept>/<str:chosen_type>/output', views.output, name='output'),
+    path('<str:chosen_dept>/<str:chosen_type>/<str:from_dt>:<str:to_dt>/', views.output, name='output'),
 ]
