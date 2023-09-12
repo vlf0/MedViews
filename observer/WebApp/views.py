a4210632c388ab486b319cc91c16d44a558172bc
vlf0 <dr.dnb2000@gmail.com>
2023-09-12 19:12:45 +0700

    Implemented and configured date choice buttons.

--- observer/WebApp/views.py
+++ observer/WebApp/views.py
@@ -10,8 +10,12 @@ from kisdb_connecting.operations import ReadyReportHTML, SelectAnswer, Queries
 
 def dept(request):
     # Fields to be sending to page (from our forms)
+    t1 = DateButtons()
     depts_list = DeptChoose()
-    return render(request=request, template_name='dept_name.html', context={'depts_list': depts_list})
+    context = {'depts_list': depts_list,
+               't1': t1
+               }
+    return render(request=request, template_name='dept_name.html', context=context)
 
 
 def ref_to_type(request):
@@ -31,18 +35,39 @@ def research_type(request, chosen_dept):
 def ref_to_output(request, chosen_dept):
     # Get research type from FORM fields
     chosen_type = request.POST.get('research_types')
-    # Get both datas from FORM fields
-    from_dt = request.POST.get('from_dt')
-    to_dt = request.POST.get('to_dt')
-    # print(from_dt, to_dt)
-    return redirect(to=output, chosen_dept=chosen_dept, chosen_type=chosen_type, from_dt=from_dt, to_dt=to_dt)
+    # Get FROM data from FORM fields by creating list generator of date values
+    from_dt = [request.POST.get(i) for i in request.POST if i in ['from_dt_month',
+                                                                  'from_dt_day',
+                                                                  'from_dt_year']
+          ]
+    # Converting date format to Postgres format (yyyy-mm-dd)
+    # because another way - change postgresql.conf file KIS DB
+    from_dt.insert(0, from_dt.pop())
+    from_dt = '-'.join(from_dt)
+    # Get TO data from FORM fields by creating list generator of date values
+    to_dt = [request.POST.get(i) for i in request.POST if i in ['to_dt_month', 'to_dt_day', 'to_dt_year']]
+    to_dt.insert(0, to_dt.pop())
+    to_dt = '-'.join(to_dt)
+    return redirect(to=output, chosen_dept=chosen_dept, chosen_type=chosen_type,
+                    from_dt=from_dt, to_dt=to_dt)
 
 
 def output(request, chosen_dept, chosen_type, from_dt, to_dt):
     if request.method == 'POST':
         chosen_type = request.POST.get('research_types')
-        from_dt = request.POST.get('from_dt')
-        to_dt = request.POST.get('to_dt')
+        # Get FROM data from FORM fields by creating list generator of date values
+        from_dt = [request.POST.get(i) for i in request.POST if i in ['from_dt_month',
+                                                                      'from_dt_day',
+                                                                      'from_dt_year']
+                   ]
+        # Converting date format to Postgres format (yyyy-mm-dd)
+        # because another way - change postgresql.conf file KIS DB
+        from_dt.insert(0, from_dt.pop())
+        from_dt = '-'.join(from_dt)
+        # Get TO data from FORM fields by creating list generator of date values
+        to_dt = [request.POST.get(i) for i in request.POST if i in ['to_dt_month', 'to_dt_day', 'to_dt_year']]
+        to_dt.insert(0, to_dt.pop())
+        to_dt = '-'.join(to_dt)
         # Handling data from db to HTML page by PANDAS
         query_text = Queries(dept=chosen_dept, research=chosen_type, from_dt=from_dt, to_dt=to_dt).ready_select()
         answer = SelectAnswer(query_text).selecting()
