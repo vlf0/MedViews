bb1a2564582fd39d35f2ce7793774f8fed84385a
AntonRyadovoy <a.ryadovoy@gmail.com>
2023-09-19 15:33:11 +0300

    displaying research names instead of numbers, new logo

--- observer/WebApp/forms.py
+++ observer/WebApp/forms.py
@@ -43,23 +43,24 @@ class DeptChoose(forms.Form):
 
 class ResearchType(forms.Form):
     """ Represent drop-down list of research name from KIS DB on the starting page. """
-    research_types = forms.ChoiceField(label='Тип исследования', choices=((1, 1), (2, 2), (3, 3), (4, 4), (5, 5)),
-                                        widget=forms.Select(attrs={'class': 'r_type-custom'})
-                                           )
+    research_types = forms.ChoiceField(label='Тип исследования',
+                                       choices=(('Лабораторные исследования', 'Лабораторные исследования'),
+                                                ('Инструментальные исследования', 'Инструментальные исследования'),
+                                                ('Процедуры и манипуляции', 'Процедуры и манипуляции'),
+                                                ('Операции', 'Операции'),
+                                                ('Консультации', 'Консультации')
+                                                ),
+                                       widget=forms.Select(attrs={'class': 'r_type-custom'})
+                                       )
 
 
 class DateButtons(forms.Form):
     """ Represent buttons of date type on the page. It is the 3rd sql condition in query. """
     from_dt = forms.DateField(label='Начало периода',
                               initial=date.today() - timedelta(days=14),
-                              widget=forms.SelectDateWidget(attrs={'class': 'datefield-custom'},
-                                                            years=range(date.today().year, date.today().year-5, -1)
-                                                            ),
+                              widget=forms.DateInput(attrs={'type': 'date', 'class': 'datefield-custom'}),
                               )
-    to_dt = forms.DateField(label='Конец периода',
-                            initial=date.today(),
-                            widget=forms.SelectDateWidget(attrs={'class': 'datefield-custom'},
-                                                          years=range(date.today().year, date.today().year-5, -1)
-                                                          ),
+    to_dt = forms.DateField(label='Конец периода', required=True, initial=date.today(),
+                            widget=forms.DateInput(attrs={'type': 'date', 'class': 'datefield-custom'}),
                             )
 
