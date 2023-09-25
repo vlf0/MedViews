424bad8d89f74fa3241129a130e8f9c814eab661
AntonRyadovoy <a.ryadovoy@gmail.com>
2023-09-25 11:21:07 +0300

    Merge css fixed elements.

--- observer/kisdb_connecting/operations.py
+++ observer/kisdb_connecting/operations.py
@@ -25,14 +25,65 @@ class Queries:
         self.to_dt = to_dt
 
     def ready_select(self):
-        if self.types_converting[self.research] == 6:
-            return f'SELECT pat_fio, pat_ib, zav, sign_dt, pat_leave_dt  FROM mm.tap' \
-                   f' WHERE sign_dt between \'{self.from_dt}\' and \'{self.to_dt}\''
+        if self.types_converting[self.research] == 7:
+            return f'SELECT concat_ws(\' \',m.surname,m.name,m.patron) AS ФИО_Пациента,' \
+                f' concat_ws (\' - \',m.num,m.YEAR) AS №ИБ,' \
+                f' mm.emp_get_fio_by_id (d.manager_emp_id ) AS Заведующий_отделением,' \
+                f' to_char(tt.sign_dt,\'DD.MM.YYYY HH24:MM:SS \') AS Дата_подписи_леч_врачом,' \
+                f' to_char (h.leave_dt,\'DD.MM.YYYY HH24:MM:SS\') AS Дата_выписки_пациента' \
+                f' FROM mm.hospdoc h' \
+                f' JOIN mm.mdoc m ON m.id = h.mdoc_id' \
+                f' JOIN mm.people p ON p.id = m.people_id' \
+                f' JOIN mm.ehr_case ec ON ec.id = h.ehr_case_id' \
+                f' LEFT JOIN mm.dept d ON d.id = h.dept_id' \
+                f' JOIN (SELECT et.ehr_case_id, et.sign_dt,' \
+                f' et.epic_code,' \
+                f' et.creator_emp_id AS emp_id' \
+                f' FROM mm.epic_text et' \
+                f' WHERE et.epic_code IN  (\'Z00.001.008\', \'Z00.001.012\', \'Z00.004.032\', \'Z00.001.014\', \'Z00.001.009\', \'Z00.001.016\')' \
+                f' AND et.create_dt >= \'01.01.2023\'' \
+                f' and et.allow_export = 2' \
+                f' AND et.ehr_case_id NOT IN' \
+                f' (SELECT hoer.ehr_case_id' \
+                f' FROM mm.hospdoc_out_emiac_remd hoer' \
+                f' WHERE hoer.create_dt >= \'01.01.2023\'' \
+                f' AND hoer.status = 1)' \
+                f' AND et.sign_dt NOTNULL) tt ON tt.ehr_case_id = h.ehr_case_id' \
+                f' AND h.leave_dt BETWEEN \'{self.from_dt}\' AND \'{self.to_dt}\'' \
+                f' AND d.name = \'{self.dept}\''
         else:
-            return f'select doc_fio, ib_num, pat_fio, research, create_dt, plan_dt FROM mm.dbkis' \
-                   f' WHERE dept = \'{self.dept}\' AND r_type = \'{self.types_converting[self.research]}\'' \
-                   f' AND create_dt between \'{self.from_dt}\' and \'{self.to_dt}\''
-
+            return f'SELECT' \
+            f' concat_ws (\' \',p.surname,p.name,p.patron) AS Назначил,'\
+            f' concat_ws (\' - \',m.num,m.YEAR) AS №ИБ,' \
+            f' concat_ws(\' \',m.surname,m.name,m.patron) AS ФИО_Пациента,' \
+            f' n.name,' \
+            f' to_char(n.create_dt, \'DD.MM.YYYY HH24:MI:SS\') AS создано,' \
+            f' to_char(n.plan_dt, \'DD.MM.YYYY HH24:MI:SS\') AS Назначено_на_дату,' \
+            f' CASE n.naz_extr_id ' \
+            f'\tWHEN \'0\' THEN \'Планово\' ' \
+            f'\tWHEN \'1\' THEN \'Экстренно\' ' \
+            f' ELSE n.naz_extr_id::TEXT ' \
+            f' END ' \
+            f' FROM mm.mdoc AS m' \
+            f' JOIN mm.hospdoc h ON h.mdoc_id = m.id' \
+            f' JOIN mm.naz n ON n.mdoc_id = m.id' \
+            f' JOIN mm.naz_type nt ON nt.id = n.naz_type_id' \
+            f' JOIN mm.naz_type_dir ntd on ntd.id = nt.naz_type_dir_id' \
+            f' JOIN mm.naz_action na ON na."id" = n.last_action_id' \
+            f' JOIN mm.emp AS em ON  em.id = n.creator_emp_id' \
+            f' JOIN mm.dept AS dp ON  dp.id = em.dept_id' \
+            f' JOIN mm.people AS p ON  p.id = em.people_id' \
+            f' JOIN mm.ehr_case ec ON ec.id = h.ehr_case_id' \
+            f' JOIN mm.naz_state ns ON ns.id = n.naz_state_id ' \
+            f' WHERE n.create_dt BETWEEN \'{self.from_dt}\' AND \'{self.to_dt}\' ' \
+            f' AND n.actual_dt BETWEEN \'{self.from_dt}\' AND \'{self.to_dt}\' ' \
+            f' AND n.plan_dt BETWEEN \'{self.from_dt}\' AND \'{self.to_dt}\' ' \
+            f' and n.create_dt between h.hosp_dt and coalesce(h.leave_dt,\'infinity\')' \
+            f' and not ntd.path <@ (select array_agg(path) from mm.naz_type_dir where tags && array[\'GROUP_PAT\'::varchar])' \
+            f' AND n.naz_view = \'{self.types_converting[self.research]}\'' \
+            f' and na.state_value not in (\'cancelled\', \'postponed\' , \'completed\')' \
+            f' AND dp.name =\'{self.dept}\''
+ 
 
 class SelectAnswer:
     """ Represent SQL query object in the text format. """
@@ -112,11 +163,11 @@ class ReadyReportHTML:
                 report = df.to_html()
                 tab = string_snippets.tab_report + report
             elif type(self.db_data) is str:
-                tab = f'\t<p class="center-top-text">{self.db_data}</p>\n'
+                tab = f'\t<p class="center-top-cnt">{self.db_data}</p>\n'
             else:
                 tab = string_snippets.system_error
         # Updating template by overwriting when get the new data from KIS
-        with open(r'D:\Programming\DjangoProjects\MedVeiws\observer\WebApp\templates\output.html', 'wt',
+        with open(r'C:\Users\adm-ryadovoyaa\Documents\DMKprojects\MedVeiws\observer\WebApp\templates\output.html', 'wt',
                   encoding='utf-8') as template:
             template.write(string_snippets.top_of_template)
             template.writelines(tab)
--- observer/kisdb_connecting/string_snippets.py
+++ observer/kisdb_connecting/string_snippets.py
@@ -50,4 +50,19 @@ system_error = '\t<p class="center-top-text">SYSTEM ERROR!</p>\n'
 
 date_validation_error = '\t\t<p class="center-top-text">Дата окончания периода не может быть меньше даты начала!</p>\n'
 
-depts_by_ids = 'SELECT d.name FROM mm.dept d'
+depts_by_ids = 'SELECT d.name FROM mm.dept d' \
+              ' WHERE d.id IN' \
+              ' (\'b99a5265-74c0-4ace-8cb3-1fe204b59aaf\',\'faf07796-1ffb-478e-a06e-717dfc46a7ec\',' \
+              ' \'1662afce-46b5-44f7-bec2-0379045f7224\', \'1fb01ccf-64b2-4fe5-b914-9c5eca23e120\',' \
+              ' \'495f76bb-0bf2-4384-acc0-0e1bdc3b7016\', \'51cf4e23-36e7-4ac8-9d93-1d31a7c2b9a2\',' \
+              ' \'7215749c-9511-46b4-b45f-2d06ea968402\', \'958fe9e3-06e8-4a61-aaa5-7839eb91a2c5\',' \
+              ' \'1d8cf85a-38b1-4220-b6b0-bdc93a32da02\', \'cad1687b-74cb-4181-833c-b8487cb3c1a2\',' \
+              ' \'84db1d24-186d-4352-93ce-fe55391da7d4\', \'bf7fd4e9-f743-4ce5-86b5-fe2f8edf6a39\',' \
+              ' \'1a368a46-9c40-4c7b-a260-01bce04ece7e\', \'52c623a6-d1c4-48ed-8644-3f7b68d78e88\',' \
+              ' \'99e772e8-ed16-4da5-a6db-aaee05bc4e66\', \'bb26d215-d705-4751-8665-a4249cc1a7a6\',' \
+              ' \'ce8a8f10-2dab-447d-b4d3-fc75939feb52\', \'ef12e48a-55d4-45fe-a448-cd7ded5eba42\',' \
+              ' \'2577fa8a-9440-46b8-805c-bb47be7f452c\', \'0f49d6a5-19c7-466a-b30a-314d2ab0c222\',' \
+              ' \'7f35c044-8375-4057-8d04-bba5573b4f85\', \'dc3ec9ee-a75c-45ab-9672-241e7618733a\',' \
+              ' \'863d276b-abe3-4b50-a920-6901292d70f0\', \'b3046a27-2ed1-4cb7-8150-e70f68e75810\',' \
+              ' \'5fe0204b-e340-486a-94db-2bcc75fc6e64\', \'7168f375-ac21-4c66-9575-f033c3ac0cd3\','\
+              ' \'85489221-1971-4a7a-9878-52ea814769ee\')'
