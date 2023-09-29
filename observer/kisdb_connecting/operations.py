a0177f549580d2b58d6b78242037d07fdd81839a
vlf0 <dr.dnb2000@gmail.com>
2023-09-29 22:45:46 +0700

    Entire OutputHTML class was reworked - now can creat separated excel file (with its saving) and HTML table from DF; also added newer class for formatting excel files automatically.

--- observer/kisdb_connecting/operations.py
+++ observer/kisdb_connecting/operations.py
@@ -2,13 +2,40 @@ import re
 import pandas as pd
 import psycopg2
 from psycopg2 import Error
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 from WebApp.models import ConnectingToKIS
 from . import string_snippets
 
 
 today = datetime.today()
 three_days = timedelta(days=3, hours=0, minutes=0, microseconds=0, milliseconds=0)
+
+
+class ReportExcelWriter:
+    """ Applying styles to dataframe going to excel file.
+    Has two methods for two different reports. """
+    @staticmethod
+    def xlsx_styles_researches(dept_name, frame):
+        writer = pd.ExcelWriter(fr'.\WebApp\static\reports\rep_{dept_name}.xlsx')
+        frame.to_excel(excel_writer=writer, sheet_name='Отчёт', engine='xlsxwriter', index=False)
+        writer.sheets['Отчёт'].set_column(0, 0, 45)
+        writer.sheets['Отчёт'].set_column(1, 1, 10)
+        writer.sheets['Отчёт'].set_column(2, 3, 45)
+        writer.sheets['Отчёт'].set_column(4, 5, 22)
+        writer.sheets['Отчёт'].set_column(6, 6, 10)
+        writer.close()
+
+    @staticmethod
+    def xlsx_styles_epicrisis(dept_name, frame):
+        writer = pd.ExcelWriter(fr'.\WebApp\static\reports\rep_{dept_name}.xlsx')
+        frame.to_excel(excel_writer=writer, sheet_name='Отчёт', engine='xlsxwriter', index=False)
+        writer.sheets['Отчёт'].set_column(0, 0, 5)
+        writer.sheets['Отчёт'].set_column(1, 1, 45)
+        writer.sheets['Отчёт'].set_column(2, 2, 10)
+        writer.sheets['Отчёт'].set_column(3, 3, 45)
+        writer.sheets['Отчёт'].set_column(4, 5, 20)
+        writer.sheets['Отчёт'].set_column(6, 6, 5)
+        writer.close()
 
 
 class Queries:
@@ -34,7 +61,7 @@ class Queries:
             return f'SELECT pat_fio, pat_ib, zav, sign_dt, pat_leave_dt FROM mm.tap' \
                    f' WHERE sign_dt between \'{self.from_dt}\' and \'{self.to_dt}\''
         else:
-            return f'select doc_fio, ib_num, pat_fio, research, create_dt, plan_dt FROM mm.dbkis' \
+            return f'select doc_fio, ib_num, pat_fio, research, create_dt, plan_dt, status FROM mm.dbkis' \
                    f' WHERE dept = \'{self.dept}\' AND r_type = \'{self.types_converting[self.research]}\'' \
                    f' AND create_dt between \'{self.from_dt}\' and \'{self.to_dt}\''
 
@@ -80,13 +107,55 @@ class SelectAnswer:
             return 'There are no any records in the BD data tab!'
 
 
-class ReadyReportHTML:
+class ReadyReport:
     """ Represent HTML page and contains selected required data.
     The data converts to HTML code by Pandas DataFrame. Data for connecting get from app DB models. """
     def __init__(self, db_data):
         self.db_data = db_data
+        if type(self.db_data) is str:
+            self.dataframe = f'\t<p class="center-top-text">{self.db_data}</p>\n'
+        elif type(self.db_data) is list and len(self.db_data) == 0:
+            self.dataframe = string_snippets.tab_done
+        else:
+            row_values = len(self.db_data[0])
+            rows_number = len(self.db_data)
+            # List of lists of data separated and grouped inside
+            data_lists = [list(map(lambda x: x[i], self.db_data)) for i in range(row_values)]
+            # DataFrame's headers names for all type of researches
+            headers_names = ['Врач', 'Номер ИБ', 'Пациент', 'Назначение',
+                             'Дата создания', 'Назначено на дату', 'Статус']
 
-    def output_data(self, error_value=None):
+            # 5 values in 1 row - if user chosen "Невыгруженные эпикризы"
+            if row_values == 5:
+                headers_names = ['ID', 'ФИО пациента', 'ИБ пациента', 'Заведующий отделения',
+                                 'Дата подписи выписного эпикриза', 'Дата выписки пациента']
+                # Adding column with ID's
+                data_lists.insert(0, [i for i in range(1, rows_number + 1)])
+            # Creating dict for DataFrame
+            data = dict(zip(headers_names, data_lists))
+            df = pd.DataFrame(data=data, index=range(1, rows_number + 1))
+            second_column_name = df.columns[1]
+            # Converting to HTML block inside the <table> tag
+            # It is middle part of body of the HTML template
+            if second_column_name == 'ФИО пациента':
+                # Adding new column contains the different between today and sign date in DF
+                df.insert(loc=6, column='Не выгружено',
+                          value=(today - df['Дата подписи выписного эпикриза'].array).days)
+            else:
+                df.columns.rename('ID', inplace=True)
+            self.dataframe = df
+
+    def to_excel(self, dept):
+        if type(self.dataframe) is not str:
+            first_column_name = self.dataframe.columns[0]
+            if first_column_name == 'ID':
+                ReportExcelWriter.xlsx_styles_epicrisis(dept_name=dept, frame=self.dataframe)
+            else:
+                ReportExcelWriter.xlsx_styles_researches(dept_name=dept, frame=self.dataframe)
+        else:
+            pass
+
+    def to_html(self, error_value=None):
         """ Prepare raw data getting from KIS DB and creating HTML template based on them. Data handled by PANDAS. """
         if error_value:
             # Date validations error text instead data table
@@ -94,74 +163,43 @@ class ReadyReportHTML:
             # Editing strings of html template
             string_snippets.top_of_template = string_snippets.top_of_template\
                 .replace('<br><br>Невыполненные {{chosen_type}} за перод с {{from_dt}} по {{to_dt}}', '')
+        elif type(self.dataframe) is str:
+            tab = self.dataframe
         else:
-            if type(self.db_data) is list and len(self.db_data) == 0:
-                tab = string_snippets.tab_done
-            elif type(self.db_data) is list and len(self.db_data) != 0:
-                row_values = len(self.db_data[0])
-                rows_number = len(self.db_data)
-                # List of lists of data separated and grouped inside
-                data_lists = [list(map(lambda x: x[i], self.db_data)) for i in range(row_values)]
-                # DataFrame's headers names for all type of researches
-                headers_names = ['Врач', 'Номер ИБ', 'Пациент', 'Назначение',
-                                 'Дата создания', 'Назначено на дату', 'Статус']
-
-                # 5 values in 1 row - if user chosen "Невыгруженные эпикризы"
-                if row_values == 5:
-                    headers_names = ['ID', 'ФИО пациента', 'ИБ пациента', 'Заведующий отделения',
-                                     'Дата подписи выписного эпикриза', 'Дата выписки пациента']
-                    # Adding list of row numbers into begin headers_names (creating own column of indexes)
-                    data_lists.insert(0, [i for i in range(1, rows_number+1)])
-                # Creating dict for DataFrame
-                data = dict(zip(headers_names, data_lists))
-                df = pd.DataFrame(data=data, index=range(1, rows_number+1))
-                second_column_name = df.head(0).columns.values[1]
-                # Converting to HTML block inside the <table> tag
-                # It is middle part of body of the HTML template
-                if second_column_name == 'ФИО пациента':
-                    # Adding new column contains the different between today and sign date in DF
-                    df.insert(loc=6, column='Не выгружено',
-                              value=(today - df['Дата подписи выписного эпикриза'].array).days)
-                    # Applying format to cells by condition
-                    report = df.to_html(formatters=
+            first_column_name = self.dataframe.columns[0]
+            if first_column_name == 'ID':
+                # Applying format to cells by condition
+                report = self.dataframe.to_html(formatters=
                                         {
                                             # Formatting by condition
                                             'ID': lambda x: f'over{x}'
-                                            if (today - df.at[x, 'Дата подписи выписного эпикриза'])
+                                            if (today - self.dataframe.at[x, 'Дата подписи выписного эпикриза'])
                                             > three_days else f'{x}',
                                             # Dates formatting a newer column "Не выгружено"
                                             'Не выгружено': lambda y: f'center{y} дней' if y not in [1, 2, 3, 4]
-                                            or y in list(range(11, 21))
-                                            and str(y)[-1] not in ['1', '2', '3', '4'] else f'center{y}'
-                                            f' день' if y == 1 else f'center{y}'
-                                            f' дня' if y in [2, 3, 4] and str(y)[-1] in ['2', '3', '4']
-                                            and y not in list(range(11, 21))
-                                            else ''
+                                            or y in list(range(11, 21)) and str(y)[-1] not in ['1', '2', '3', '4']
+                                            else f'center{y}'
+                                            f' день' if y == 1 else f'center{y}' f' дня' if y in [2, 3, 4]
+                                            and str(y)[-1] in ['2', '3', '4'] and y not in list(range(11, 21)) else ''
                                         },
                                         justify='center', index=False)
 
-                    report = report.replace('<td>center', '<td style="text-align: center;">')
-                    # Changing color and style in the all ID's cells
-                    report = re.sub(r'<tr>\s*<td>', '<tr>\n\t  <td bgcolor="#be875c" style="text-align: center;">',
-                                    report)
-                    # Applying entire row color style where is text "over" (it is condition for dates comparison)
-                    report = re.sub(r'<tr>\s*<td bgcolor="#be875c" style="text-align: center;">over',
-                                    '<tr bgcolor="yellow">\n\t  <td bgcolor="#be875c" style="text-align: center;">',
-                                    report)
-                else:
-                    df.columns.rename('ID', inplace=True)
-                    report = df.to_html(justify="center")
-                    # Adding own class for further applying css for column "ID"
-                    report = re.sub(r'<tr style="text-align: center;">\s*<th>ID',
-                                    '<tr style="text-align: center;">\n\t  <th class="index-name">ID', report)
-                # Result of creating dataframe and formatting to HTML
-                tab = string_snippets.tab_report + string_snippets.tab_table + report + string_snippets.tab_table_end
+                report = report.replace('<td>center', '<td style="text-align: center;">')
+                # Changing color and style in the all ID's cells
+                report = re.sub(r'<tr>\s*<td>', '<tr>\n\t  <td bgcolor="#be875c" style="text-align: center;">',
+                                report)
+                # Applying entire row color style where is text "over" (it is condition for dates comparison)
+                report = re.sub(r'<tr>\s*<td bgcolor="#be875c" style="text-align: center;">over',
+                                '<tr bgcolor="yellow">\n\t  <td bgcolor="#be875c" style="text-align: center;">',
+                                report)
 
-            elif type(self.db_data) is str:
-                tab = f'\t<p class="center-top-text">{self.db_data}</p>\n'
             else:
-                tab = string_snippets.system_error
-        # Updating template by overwriting when get the new data from KIS
+                report = self.dataframe.to_html(justify="center")
+                # Adding own class for further applying css for column "ID"
+                report = re.sub(r'<tr style="text-align: center;">\s*<th>ID',
+                                '<tr style="text-align: center;">\n\t  <th class="index-name">ID', report)
+            # Result of creating dataframe and formatting to HTML
+            tab = string_snippets.tab_report + string_snippets.tab_table + report + string_snippets.tab_table_end
         with open(r'D:\Programming\DjangoProjects\MedVeiws\observer\WebApp\templates\output.html', 'wt',
                   encoding='utf-8') as template:
             template.write(string_snippets.top_of_template)
